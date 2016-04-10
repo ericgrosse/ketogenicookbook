98170be47e6c4c83c131c72d3001b37a7a7adcb8
ericgrosse <ericgrosse1@gmail.com>
2016-04-10 10:28:25 -0400

    The changes keep coming

--- js/script.js
+++ js/script.js
@@ -28,3 +28,30 @@ $window.on('scroll resize', checkIfInView);
 $window.trigger('scroll'); // trigger the scroll event as soon as the DOM is ready
 
 });*/
+
+$(document).ready(function() {
+
+	var $section6ImgLeft = $('.section-6-image-left');
+	var $section7TextLeft = $('.section-7-content-left h1'); 
+
+	var $section6ImgRight = $('.section-6-image-right');
+	var $section7TextRight = $('.section-7-content-right h1'); 
+
+	$section6ImgLeft.hover(
+		function() {
+			$section7TextLeft.css('text-decoration', 'underline');
+		},
+		function() {
+			$section7TextLeft.css('text-decoration', '');
+		}
+	);
+
+	$section6ImgRight.hover(
+		function() {
+			$section7TextRight.css('text-decoration', 'underline');
+		},
+		function() {
+			$section7TextRight.css('text-decoration', '');
+		}
+	);
+});
