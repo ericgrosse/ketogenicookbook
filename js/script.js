5c3816e02b878c61b9c991b7e4918c48cc12a2da
ericgrosse <ericgrosse1@gmail.com>
2016-04-10 16:52:39 -0400

    More font adjustments

--- js/script.js
+++ js/script.js
@@ -1,34 +1,3 @@
-/*$(document).ready(function() {
-
-var $animatedIcons = $(); // Add classnames here
-var $window = $(window);
-
-function checkIfInView() {
-	var windowHeight = $window.height();
-	var windowTopPosition = $window.scrollTop();
-	var windowBottomPosition = windowTopPosition + windowHeight;
-
-	$.each($animatedIcons, function() {
-		var $element = $(this);
-		var elementHeight = $element.outerHeight();
-		var elementTopPosition = $element.offset().top;
-		var elementBottomPosition = elementTopPosition + elementHeight;
-
-		// check to see if this current container is within viewport
-		if( (elementBottomPosition >= windowTopPosition) && (elementTopPosition <= windowBottomPosition) ) {
-			$element.addClass('in-view');
-		}
-		else {
-			$element.removeClass('in-view');
-		}
-	});
-}
-
-$window.on('scroll resize', checkIfInView);
-$window.trigger('scroll'); // trigger the scroll event as soon as the DOM is ready
-
-});*/
-
 $(document).ready(function() {
 
 	/* Animate image when scrolling to it */
@@ -58,8 +27,6 @@ $(document).ready(function() {
 		});
 
 		/* Show the navbar if scrolling upwards */
-
-		console.log(windowTopPosition);
 
 		//if(windowTopPosition > 62) {
 			if(windowTopPosition < lastScrollTop) {
