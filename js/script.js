f0a03c1d333ea9b83aa2b6c454480f72256b87cf
ericgrosse <ericgrosse1@gmail.com>
2016-04-10 13:42:19 -0400

    Added animations

--- js/script.js
+++ js/script.js
@@ -31,27 +31,76 @@ $window.trigger('scroll'); // trigger the scroll event as soon as the DOM is rea
 
 $(document).ready(function() {
 
-	var $section6ImgLeft = $('.section-6-image-left');
-	var $section7TextLeft = $('.section-7-content-left h1'); 
+	/* Animate image when scrolling to it */
 
-	var $section6ImgRight = $('.section-6-image-right');
-	var $section7TextRight = $('.section-7-content-right h1'); 
+	var $animatedIcons = $('.section-6'); // Add classnames here
+	var $window = $(window);
+	var lastScrollTop = 0;
+
+	function checkIfInView() {
+		var windowHeight = $window.height();
+		var windowTopPosition = $window.scrollTop();
+		var windowBottomPosition = windowTopPosition + windowHeight;
+
+		$.each($animatedIcons, function() {
+			var $element = $(this);
+			var elementHeight = $element.outerHeight();
+			var elementTopPosition = $element.offset().top;
+			var elementBottomPosition = elementTopPosition + elementHeight;
+
+			// check to see if this current container is within viewport
+			if( /*(elementBottomPosition >= windowTopPosition) &&*/ (elementTopPosition <= windowBottomPosition) ) {
+				$element.addClass('in-view');
+			}
+			else {
+				$element.removeClass('in-view');
+			}
+		});
+
+		/* Show the navbar if scrolling upwards */
+
+		console.log(windowTopPosition);
+
+		//if(windowTopPosition > 62) {
+			if(windowTopPosition < lastScrollTop) {
+				$('.section-1 .header').removeClass('scroll-down').addClass('scroll-up');
+			}
+			else {
+				$('.section-1 .header').removeClass('scroll-up').addClass('scroll-down');
+			}			
+		//}
+		/*else {
+			$('.section-1 .header').removeClass('scoll-down').removeClass('scroll-up');
+		}*/
+
+		lastScrollTop = windowTopPosition;
+	}
+
+	$window.on('scroll', checkIfInView);
+
+	/* Underline text when hovering over unrelated image */
+
+	var $section6ImgLeft = $('.section-6 .image-left');
+	var $section6TextLeft = $('.section-6 .content-left h1'); 
+
+	var $section6ImgRight = $('.section-6 .image-right');
+	var $section6TextRight = $('.section-6 .content-right h1'); 
 
 	$section6ImgLeft.hover(
 		function() {
-			$section7TextLeft.css('text-decoration', 'underline');
+			$section6TextLeft.addClass('underline');
 		},
 		function() {
-			$section7TextLeft.css('text-decoration', '');
+			$section6TextLeft.removeClass('underline');
 		}
 	);
 
 	$section6ImgRight.hover(
 		function() {
-			$section7TextRight.css('text-decoration', 'underline');
+			$section6TextRight.addClass('underline');
 		},
 		function() {
-			$section7TextRight.css('text-decoration', '');
+			$section6TextRight.removeClass('underline');
 		}
 	);
 });
